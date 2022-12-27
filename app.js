d34586077e14f4d6e18e442ff1d4b75fd8dd589e
Glitch (wa-bot-addfa) <none>
2022-12-27 20:03:33 +0000

    fixing

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { isBot } = require('./src/message')
 const { writeUrl } = require('./src/qrUrl')
 
 const client = new waWeb.Client({
-<<<<<<< HEAD
     authStrategy: new waWeb.LocalAuth({ clientId: 'client-one' }),
     puppeteer: {
       args: ['--no-sandbox'],
@@ -13,16 +12,8 @@ const client = new waWeb.Client({
 })
 
 client.on('qr', qr => {
-    // qrTerminal.generate(qr, { small: true })
-  console.log(qr)
-=======
-    // authStrategy: new waWeb.LocalAuth({ clientId: 'client-one' })
-})
-
-client.on('qr', async qr => {
     const urlQr = await qrcode.toDataURL(qr)
     writeUrl(urlQr)
->>>>>>> 18804bd9760f9cde3a8be12f7246364f5556d33d
 })
 
 client.on('ready', () => console.log('Client Is Ready!'))
