a3ec4f9241b5602846b43564863b094289f18104
Glitch (wa-bot-addfa) <none>
2022-12-27 20:20:44 +0000

    🚘🐢 Checkpoint ./app.js:52591922/1627 ./data/qrUrl.txt:52591922/6382

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const waWeb = require('whatsapp-web.js')
-const qrcode = require('qrcode')
+const waWeb = require("whatsapp-web.js");
+const qrcode = require("qrcode");
 
-const { isBot } = require('./src/message')
-const { writeUrl } = require('./src/qrUrl')
+const { isBot } = require("./src/message");
+const { writeUrl } = require("./src/qrUrl");
 
 const client = new waWeb.Client({
-    authStrategy: new waWeb.LocalAuth({ clientId: 'client-one' }),
-    puppeteer: {
-      args: ['--no-sandbox'],
-    }
-})
+  authStrategy: new waWeb.LocalAuth({ clientId: "client-one" }),
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
+});
 
-client.on('qr', async qr => {
-    const urlQr = await qrcode.toDataURL(qr)
-    writeUrl(urlQr)
-})
+client.on("qr", async (qr) => {
+  const urlQr = await qrcode.toDataURL(qr);
+  writeUrl(urlQr);
 
-client.on('ready', () => console.log('Client Is Ready!'))
+  console.log(urlQr);
+});
+
+client.on("ready", () => console.log("Client Is Ready!"));
 
 // client.on('message', message => {
 //     const replying = isBot(message.body)
 //     if (replying) message.reply(replying)
 // })
 
-client.on('message_create', message => {
-    const replying = isBot(message.body)
-    if (replying) message.reply(replying)
-})
+client.on("message_create", (message) => {
+  const replying = isBot(message.body);
+  if (replying) message.reply(replying);
+});
 
-client.initialize()
+client.initialize();
