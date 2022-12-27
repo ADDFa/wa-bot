1b1e4c3f5bcbdaa73d7fdaa363353ea3e931911a
ADDFa <fratamaadha18@gmail.com>
2022-12-27 20:18:07 +0700

    01 fix

---
+++ app.js
@@ -0,0 +1,21 @@
+const waWeb = require('whatsapp-web.js')
+const qrTerminal = require('qrcode-terminal')
+
+const { isBot } = require('./src/message')
+
+const client = new waWeb.Client({
+    authStrategy: new waWeb.LocalAuth({ clientId: 'client-one' })
+})
+
+client.on('qr', qr => {
+    qrTerminal.generate(qr, { small: true })
+})
+
+client.on('ready', () => console.log('Client Is Ready!'))
+
+client.on('message', message => {
+    const replying = isBot(message.body)
+    if (replying) message.reply(replying)
+})
+
+client.initialize()
---
+++ src/message.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+const getReply = message => {
+    const replys = JSON.parse(fs.readFileSync('./data/message.json', 'utf-8'))
+    const issetReply = replys.find(e => e[message])
+
+    const reply = (issetReply) ? issetReply[message] : replys.find(e => e['unknown']).unknown
+
+    return `Terimakasih Atas Pertanyaan Anda\n\n${reply.join('')}`
+}
+
+const isBot = message => {
+    const messages = message.split(':')
+    const isAsistant = messages[0].trim().toLowerCase() === 'asisten'
+
+    if (!isAsistant) return false
+
+    // ambil balasan jika merupakan asisten bot
+    return getReply(messages[1].trim())
+}
+
+// message => asisten: <<< name >>>
+module.exports = {
+    isBot
+}
