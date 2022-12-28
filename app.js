e69f1265e00a4effdde04074a0d61976ae781a0d
ADDFa <fratamaadha18@gmail.com>
2022-12-28 09:41:47 +0700

    menambahkan fitur asisten-jawab

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-const waWeb = require("whatsapp-web.js");
-const qrcode = require("qrcode");
+const waWeb = require("whatsapp-web.js")
+const qrcode = require("qrcode")
 
-const { isBot } = require("./src/message");
-const { writeUrl } = require("./src/qrUrl");
+const { isBot, createMsgBot } = require("./src/message")
+const { writeUrl } = require("./src/qrUrl")
 
 const client = new waWeb.Client({
-  authStrategy: new waWeb.LocalAuth({ clientId: "client-one" }),
-  puppeteer: {
-    args: ["--no-sandbox"],
-  },
-});
+    authStrategy: new waWeb.LocalAuth({ clientId: "client-one" }),
+    puppeteer: {
+        args: ["--no-sandbox"],
+    }
+})
 
 client.on("qr", async (qr) => {
-  const urlQr = await qrcode.toDataURL(qr);
-  writeUrl(urlQr);
+    const urlQr = await qrcode.toDataURL(qr)
+    writeUrl(urlQr)
 
-  console.log(urlQr);
-});
+    console.log(urlQr)
+})
 
-client.on("ready", () => console.log("Client Is Ready!"));
+client.on("ready", () => console.log("Client Is Ready!"))
 
 client.on('message', message => {
     const replying = isBot(message.body)
     if (replying) message.reply(replying)
 })
 
-// client.on("message_create", (message) => {
-//   const replying = isBot(message.body);
-//   if (replying) message.reply(replying);
-// });
+client.on("message_create", message => {
+    const isMsgBot = createMsgBot({
+        fromMe: message.fromMe,
+        message: message.body
+    })
 
-client.initialize();
+    if (isMsgBot) message.reply(isMsgBot)
+})
+
+client.initialize()
--- src/message.js
+++ src/message.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 
-const getReply = message => {
+const getReply = (message, useThanks = true) => {
+    const validMsg = message.match(/[a-z]+/g).join(' ')
+
     const replys = JSON.parse(fs.readFileSync('./data/message.json', 'utf-8'))
-    const issetReply = replys.find(e => e[message])
+    const issetReply = replys.find(e => e[validMsg])
 
-    const reply = (issetReply) ? issetReply[message] : replys.find(e => e['unknown']).unknown
+    const reply = (issetReply) ? issetReply[validMsg] : replys.find(e => e['unknown']).unknown
 
-    return `Terimakasih Atas Pertanyaan Anda\n\n${reply.join('')}`
+    if (useThanks) return `Terimakasih Atas Pertanyaan Anda\n\n${reply.join('')}`
+
+    return reply.join('')
 }
 
 const isBot = message => {
@@ -16,10 +20,18 @@ const isBot = message => {
     if (!isAsistant) return false
 
     // ambil balasan jika merupakan asisten bot
-    return getReply(messages[1].match(/[a-z]+/g).join(' '))
+    return getReply(messages[1])
+}
+
+const createMsgBot = ({ fromMe, message }) => {
+    if (!fromMe) return false
+
+    const replys = `${getReply('bot')}${getReply('list', false)}`
+    if (message.toLowerCase() === 'asisten-jawab') return replys
 }
 
 // message => asisten: <<< name >>>
 module.exports = {
-    isBot
+    isBot,
+    createMsgBot
 }
